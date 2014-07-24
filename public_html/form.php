0230a55755d48bb62c6cfb5c363822a1ecc4d344
pristefan <pris.stefan@gmail.com>
2014-07-24 15:50:45 +0300

    last changes with of the Login/SignUp form.

--- public_html/form.php
+++ public_html/form.php
@@ -1,7 +1,11 @@
 <?php
 
-
-    echo 'Hello ' . htmlspecialchars($_POST["lName"]) . ' ' . htmlspecialchars($_POST["fName"]) .
+if (!empty($_POST['lName']) &&
+        !empty($_POST["fName"]) &&
+        !empty($_POST['email'])) {
+    echo 'Hello ' . htmlspecialchars($_POST["fName"]) . ' ' . htmlspecialchars($_POST["lName"]) .
     "\n\nYour email address is " . htmlspecialchars($_POST["email"]);
-
+}
+if (!empty($_POST['user'])) {
     echo 'Hello ' . htmlspecialchars($_POST["user"]);
+}
